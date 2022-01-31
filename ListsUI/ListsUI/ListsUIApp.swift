d1019470b8f4a2ed18afc1a0f15c95ebd9fc78d8
Paweł Brzozowski <mac@192.168.1.4>
2022-01-31 18:42:05 +0100

    created lists project

---
+++ ListsUI/ListsUI/ListsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsUIApp.swift
+//  ListsUI
+//
+//  Created by Paweł Brzozowski on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
