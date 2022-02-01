52f23bdca8407d4eaf79f238e1bdb016a3b46bfa
Paweł Brzozowski <mac@192.168.1.4>
2022-02-01 17:23:39 +0100

    created quotes

---
+++ Quotes/Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Paweł Brzozowski on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
