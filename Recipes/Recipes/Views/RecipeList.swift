5f07068754ed7649844e9f6d8db5549561a88ee6
Paweł Brzozowski <mac@192.168.1.4>
2022-02-02 15:42:53 +0100

    added lazyVstack

--- Recipes/Recipes/Views/RecipeList.swift
+++ Recipes/Recipes/Views/RecipeList.swift
@@ -15,24 +15,32 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) {recipe in
+            VStack(alignment: .leading) {
+                Text("All Recipes").bold().font(.largeTitle).padding(.top, 40)
                 
-                NavigationLink {
-                    RecipeDetailView(recipe: recipe)
-                } label: {
-                    HStack(alignment: .center, spacing: 20.0) {
-                        Image(recipe.image).resizable().scaledToFit().frame(width: 50, height: 50, alignment: .center).clipped().cornerRadius(5)
-                        Text(recipe.name)
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes) {recipe in
+                            NavigationLink {
+                                RecipeDetailView(recipe: recipe)
+                            } label: {
+                                HStack(alignment: .center, spacing: 20.0) {
+                                    Image(recipe.image).resizable().scaledToFit().frame(width: 50, height: 50, alignment: .center).clipped().cornerRadius(5)
+                                    Text(recipe.name).foregroundColor(.black)
+                                }
+                            }
+                            
+                        }.listStyle(PlainListStyle()).navigationBarHidden(true)
                     }
                 }
-                
-            }.listStyle(PlainListStyle()).navigationTitle("All Recipes")
+            }.padding(.leading)
+            
         }
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        RecipeListView().environmentObject(RecipeModel())
     }
 }
