145e558726fd1f25adc7bb50506d66593c20790d
Paweł Brzozowski <mac@192.168.1.4>
2022-02-02 12:23:47 +0100

    added tab view

---
+++ Recipes/Recipes/Views/RecipeTabView.swift
@@ -0,0 +1,34 @@
+//
+//  RecipeTabView.swift
+//  Recipes
+//
+//  Created by Paweł Brzozowski on 02/02/2022.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View").tabItem {
+                VStack {
+                    Image(systemName: "star.fill")
+                    Text("Featured")
+                }
+            }
+            RecipeListView().tabItem {
+                VStack {
+                    Image(systemName: "list.bullet")
+                    Text("List")
+                }
+            }
+            
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
