ba4c06f4d01ecb1a21e37eb1b6ab849c25ef91b8
PawelB <pawel.brzozowski@appunite.com>
2022-04-13 12:21:42 +0200

    Created TicTacToe

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Paweł Brzozowski on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
