7872a6db15b14c56c6d20c5da9dd3ba8b78b833d
PawelB <pawel.brzozowski@appunite.com>
2022-04-13 14:20:39 +0200

    Basic Game Finished

---
+++ TicTacToe/TicTacToe/Alerts.swift
@@ -0,0 +1,8 @@
+//
+//  Alerts.swift
+//  TicTacToe
+//
+//  Created by Paweł Brzozowski on 13/04/2022.
+//
+
+import Foundation
--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -37,10 +37,22 @@ struct ContentView: View {
                             moves[index] = Move(player: .human, boardIndex: index)
                             isGameBoardDisabled = true
                             
+                            if checkWinCondition(for: .human, in: moves) {
+                                print("Human Wins")
+                            }
+                            
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
                                 let computerPosition = determineComputerMovesPosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
                                 isGameBoardDisabled = false
+                                
+                                if checkWinCondition(for: .computer, in: moves) {
+                                    print("Computer Wins")
+                                }
+                            }
+                            
+                            if checkForDraw(in: moves) {
+                                print("It's a draw!")
                             }
                         }
                     }
@@ -51,6 +63,19 @@ struct ContentView: View {
             .padding()
 
         }
+    }
+    
+    func checkForDraw(in moves: [Move?]) -> Bool {
+        return moves.compactMap({$0}).count == 9
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6], [1,4,7], [2,5,8],[0,4,8],[2,4,6]]
+        let playerMoves = moves.compactMap({$0}).filter({$0.player == player})
+        let playerPosition = Set(playerMoves.map({$0.boardIndex}))
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPosition) {return true}
+        return false
     }
     
     func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
