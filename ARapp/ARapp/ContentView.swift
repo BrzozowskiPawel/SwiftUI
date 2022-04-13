0384927b9b848b2be2242014cd6ce42ff64c6292
PawelB <pawel.brzozowski@appunite.com>
2022-04-13 19:33:55 +0200

    Added Models (with choosing)

--- ARapp/ARapp/ContentView.swift
+++ ARapp/ARapp/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    private var models: [String] = {
+        let filemanager = FileManager.default
+        guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath: path) else {
+            return []
+        }
+        var avalibleModels: [String] = []
+        for file in files where file.hasSuffix("usdz"){
+            let name = file.replacingOccurrences(of: ".usdz", with: "")
+            avalibleModels.append(name)
+        }
+        return avalibleModels
+    }()
+    
     var body: some View {
-        return Text("Hello World ❤️")
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            ModelPickerView(models: models)
+        }
     }
 }
 
@@ -20,14 +36,36 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        
-        
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    var models: [String]
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0..<self.models.count) { index in
+                    Button {
+                        print("HELLO")
+                    } label: {
+                        Image(self.models[index])
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .aspectRatio(1/1, contentMode: .fit)
+                            .background(Color.clear)
+                            .cornerRadius(15)
+                    }.buttonStyle(.plain)
+
+                }
+            }
+        }.padding(20)
+            .background(Color.black.opacity(0.5))
+    }
 }
 
 #if DEBUG
