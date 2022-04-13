791c6068de497efa95b14721d53da167d7b35123
PawelB <pawel.brzozowski@appunite.com>
2022-04-13 19:54:00 +0200

    Placment work

--- ARapp/ARapp/ContentView.swift
+++ ARapp/ARapp/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    @State private var isPlacementEnabled = false
+    
     private var models: [String] = {
         let filemanager = FileManager.default
         guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath: path) else {
@@ -25,8 +27,13 @@ struct ContentView : View {
     var body: some View {
         ZStack(alignment: .bottom) {
             ARViewContainer()
-            ModelPickerView(models: models)
-            PlacentButtonsView()
+            
+            if self.isPlacementEnabled {
+                PlacentButtonsView(isPlacementEnabled: self.$isPlacementEnabled)
+            } else {
+                ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, models: models)
+            }
+            
         }
     }
 }
@@ -46,13 +53,16 @@ struct ARViewContainer: UIViewRepresentable {
 }
 
 struct ModelPickerView: View {
+    @Binding var isPlacementEnabled: Bool
     var models: [String]
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 30) {
                 ForEach(0..<self.models.count) { index in
                     Button {
                         print("HELLO")
+                        self.isPlacementEnabled = true
                     } label: {
                         Image(self.models[index])
                             .resizable()
@@ -70,31 +80,37 @@ struct ModelPickerView: View {
 }
 
 struct PlacentButtonsView: View {
+    @Binding var isPlacementEnabled: Bool
     var body: some View {
         HStack {
-        Button {
-            print("DEBUG CANCEL")
-        } label: {
-            Image(systemName: "xmark")
-                .frame(width: 60, height: 60)
-                .font(.title)
-                .foregroundColor(.red)
-                .background(Color.white.opacity(0.75))
-                .cornerRadius(30)
-                .padding(20)
-        }
-        Button {
-            print("DEBUG MODEL CONFIM")
-        } label: {
-            Image(systemName: "checkmark")
-                .frame(width: 60, height: 60)
-                .font(.title)
-                .foregroundColor(.green)
-                .background(Color.white.opacity(0.75))
-                .cornerRadius(30)
-                .padding(20)
+            Button {
+                print("DEBUG CANCEL")
+                resetPlacementParameters()
+            } label: {
+                Image(systemName: "xmark")
+                    .frame(width: 60, height: 60)
+                    .font(.title)
+                    .foregroundColor(.red)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            }
+            Button {
+                print("DEBUG MODEL CONFIM")
+                resetPlacementParameters()
+            } label: {
+                Image(systemName: "checkmark")
+                    .frame(width: 60, height: 60)
+                    .font(.title)
+                    .foregroundColor(.green)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            }
         }
     }
+    func resetPlacementParameters() {
+        self.isPlacementEnabled = false
     }
 }
 
