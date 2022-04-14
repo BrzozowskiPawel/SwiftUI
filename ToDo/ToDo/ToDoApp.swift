709d01e9f4d69bf2a4801a1585bb0b8c03d5ae15
PawelB <pawel.brzozowski@appunite.com>
2022-04-14 11:57:34 +0200

    ToDO app

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Paweł Brzozowski on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
