17484f9d851ddc1bf926a87413fc0838bb9196c5
PawelB <pawel.brzozowski@appunite.com>
2022-04-15 17:33:47 +0200

    Animations App

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Paweł Brzozowski on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
