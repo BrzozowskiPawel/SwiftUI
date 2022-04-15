3a6534b31df412f85cc0f092bcd168a486cff503
PawelB <pawel.brzozowski@appunite.com>
2022-04-15 17:50:11 +0200

    Animation Basics

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    @State private var xValue = 0.0
+    @State private var yValue = 0.0
+    @State private var zValue = 0.0
+    @State private var duration = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("X value: \(xValue)")
+            Slider(value: $xValue, in: 0...1)
+            Text("Y value: \(yValue)")
+            Slider(value: $yValue, in: 0...1)
+            Text("Z value: \(zValue)")
+            Slider(value: $zValue, in: 0...1)
+            Text("Duration: \(duration)")
+            Slider(value: $duration, in: 0...4)
+            Button("Tap Me") {
+                withAnimation(Animation.easeInOut(duration: duration)) {
+                    animationAmount += 360
+                }
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x: xValue, y: yValue, z: zValue))
+        }
+        
     }
 }
 
