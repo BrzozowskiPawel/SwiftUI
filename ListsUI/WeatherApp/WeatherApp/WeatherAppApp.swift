1b4847dbf432388aad90d83dca612b33a6ede239
PawelB <pawel.brzozowski@appunite.com>
2022-04-19 15:59:43 +0200

    weatherApp

---
+++ ListsUI/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Paweł Brzozowski on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
