dbab706420ce794b3fdf73142c35bcd543f7c72a
PawelB <pawel.brzozowski@appunite.com>
2022-04-19 17:48:00 +0200

    App Finished

--- ListsUI/WeatherApp/WeatherApp/View/WeatcherView.swift
+++ ListsUI/WeatherApp/WeatherApp/View/WeatcherView.swift
@@ -54,6 +54,34 @@ struct WeatcherView: View {
                 
             }.padding()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: weather.main.tempMin.roundDouble2Str() + "°")
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: weather.main.tempMax.roundDouble2Str() + "°")
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: weather.wind.speed.roundDouble2Str() + "m/s")
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: weather.main.humidity.roundDouble2Str() + "%")
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
