d7bb7ee0be126967c886a96aa63b58073c588b02
PawelB <pawel.brzozowski@appunite.com>
2022-04-19 16:06:43 +0200

    loaction manager

---
+++ ListsUI/WeatherApp/WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,36 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Paweł Brzozowski on 19/04/2022.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while getting lcoation: \(error)")
+        isLoading = false
+    }
+}
