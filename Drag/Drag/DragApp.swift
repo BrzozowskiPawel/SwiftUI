9e598e6752dade81bd29d7ccfff9a7c2d35fcb8f
PawelB <pawel.brzozowski@appunite.com>
2022-04-19 10:09:37 +0200

    Created drag apication

---
+++ Drag/Drag/DragApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragApp.swift
+//  Drag
+//
+//  Created by Paweł Brzozowski on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
