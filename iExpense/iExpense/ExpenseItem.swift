32150ddd77b3bb76eb0d70884f6fcc807c90cd52
PawelB <pawel.brzozowski@appunite.com>
2022-04-20 20:34:57 +0200

    Added iExpense

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Paweł Brzozowski on 20/04/2022.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Paweł Brzozowski on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
