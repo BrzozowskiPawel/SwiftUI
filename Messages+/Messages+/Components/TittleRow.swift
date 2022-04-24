11df3e4164feb20d23d801cc973f3e192fecf58a
PawelB <pawel.brzozowski@appunite.com>
2022-04-24 21:03:06 +0200

    Header Row

---
+++ Messages+/Messages+/Components/TittleRow.swift
@@ -0,0 +1,49 @@
+//
+//  TittleRow.swift
+//  Messages+
+//
+//  Created by Paweł Brzozowski on 24/04/2022.
+//
+
+import SwiftUI
+
+struct TittleRow: View {
+    var imageUrl = URL(string: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80")
+    
+    var name = "Sarah Smith"
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            AsyncImage(url: imageUrl) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(50)
+            } placeholder: {
+                ProgressView()
+            }
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.title).bold()
+                Text("Online")
+                    .font(.caption)
+                    .foregroundColor(.green)
+                
+            }.frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "phone.fill")
+                .foregroundColor(.gray)
+                .padding(10)
+                .background(.white)
+                .cornerRadius(50)
+        }.padding()
+    }
+}
+
+struct TittleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TittleRow()
+            .background(Color("Peach"))
+    }
+}
