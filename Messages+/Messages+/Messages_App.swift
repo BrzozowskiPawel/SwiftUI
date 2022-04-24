545de16a9cdc718202b9b2b64253834fe01c6a6f
PawelB <pawel.brzozowski@appunite.com>
2022-04-24 20:45:59 +0200

    Messages

---
+++ Messages+/Messages+/Messages_App.swift
@@ -0,0 +1,17 @@
+//
+//  Messages_App.swift
+//  Messages+
+//
+//  Created by Paweł Brzozowski on 24/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Messages_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
